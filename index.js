1ff692a23d44e00d1de2319be3589d7c42af3b18
kamardjoba <Nazarlymar152@gmail.com>
2024-09-15 19:53:18 +0200

    initial

--- index.js
+++ index.js
@@ -845,7 +845,7 @@ bot.onText(/\/start(?: (.+))?/, async (msg, match) => {
       }
     }
 
-    const appUrl = `https://octies.org/?userId=${userId}`;
+    const appUrl = `https://bomboklad.online/?userId=${userId}`;
     const channelUrl = `https://t.me/octies_community`;
 
     const imagePath = path.join(__dirname, 'images', 'Octies_bot_logo.png');
